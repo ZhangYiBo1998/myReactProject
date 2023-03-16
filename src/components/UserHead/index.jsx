96c322174fa06b8f1e7c2cbcd6cea41590c824b3
“ZhangYiBo1998” <27582180@qq.com>
2023-03-16 11:51:14 +0800

    将导航菜单更新到antdv5版本

--- src/components/UserHead/index.jsx
+++ src/components/UserHead/index.jsx
@@ -1,52 +1,62 @@
 import { LogoutOutlined, MoreOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
-import { Avatar, Dropdown, Menu } from 'antd';
+import { Avatar, Dropdown, Space } from 'antd';
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom'; //使普通组件也能使用路由组件的参数
 import { getCookie } from '../../utils';
 import ReactLogo from '../ReactLogo';
 import './index.css';
+// 加载图片文件夹，为动态加载本地图片做准备
+const images = require.context('../../img', true);
+
 
 function UserHead(props) {
     const [state] = useState({
-        userName: getCookie('userInfo').userName,
+        userInfo:getCookie('userInfo'),
     })
 
-    function handleMenuClick(e) {
-        // console.log('click', e);
-        if (e.key === "userCenter") {
+    function handleMenuClick(key) {
+        console.log('click', key);
+
+        if (key === "userCenter") {
             props.history.push('/userCenter');
             return;
         }
-        if (e.key === "settingCenter") {
+        if (key === "settingCenter") {
             props.history.push('/settingCenter');
             return;
         }
-        if (e.key === "Github") {
+        if (key === "Github") {
             window.open('https://github.com/ZhangYiBo1998/my_react_project');
             return;
         }
-        if (e.key === "signOut") {
+        if (key === "signOut") {
             props.history.push('/login');
             return;
         }
     }
 
-    const menu = (
-        <Menu onClick={handleMenuClick}>
-            <Menu.Item key="userCenter" icon={<UserOutlined />}>
-                个人中心
-            </Menu.Item>
-            <Menu.Item key="settingCenter" icon={<SettingOutlined />}>
-                设置
-            </Menu.Item>
-            <Menu.Item key="Github" icon={<SettingOutlined />}>
-                Github
-            </Menu.Item>
-            <Menu.Item key="signOut" icon={<LogoutOutlined />}>
-                退出
-            </Menu.Item>
-        </Menu>
-    );
+    const items = [
+        {
+          label: (<span onClick={e=>{handleMenuClick('userCenter')}}>个人中心</span>),
+          key: 'userCenter',
+          icon:<UserOutlined />,
+        },
+        {
+          label: <span onClick={e=>{handleMenuClick('settingCenter')}}>设置</span>,
+          key: 'settingCenter',
+          icon:<SettingOutlined />,
+        },
+        {
+          label: <span onClick={e=>{handleMenuClick('Github')}}>Github</span>,
+          key: 'Github',
+          icon:<SettingOutlined />,
+        },
+        {
+          label: <span onClick={e=>{handleMenuClick('signOut')}}>退出帐号</span>,
+          key: 'signOut',
+          icon:<LogoutOutlined />,
+        },
+      ];
 
     return (
         <div className='UserHead'>
@@ -56,19 +66,24 @@ function UserHead(props) {
             <div className='right'>
                 <div className='userInfo'>
                     <div className='imgBox'>
-                        {/* <img src="/" alt="头像" /> */}
-                        <Avatar size={50} icon={<UserOutlined />} />
+                        {/* 设置头像 因为是本地图片，只能使用此方法动态加载，网络图片直接可以使用链接*/}
+                        {
+                            state.userInfo.img?(
+                                <img src={images(`${state.userInfo.img}`).default} style={{width:"50px",height:"50px",borderRadius:"50%"}} alt="头像" />
+                            ):(
+                                <Avatar size={50} icon={<UserOutlined />} />
+                            )
+                        }
                     </div>
                     <div className='userName'>
-                        {/* <Dropdown.Button overlay={menu} trigger={['click']} icon={<MoreOutlined className='more' />}>
-                            {state.userName}
-                        </Dropdown.Button> */}
-                        <Dropdown overlay={menu} trigger={['click']}>
-                            <span style={{ cursor: 'pointer' }} onClick={e => e.preventDefault()}>
-                                {state.userName} <MoreOutlined className='more' />
+                        <Dropdown menu={{items}} trigger={['click']}>
+                            <span>
+                                <Space>
+                                    {state.userInfo.userName}
+                                    <MoreOutlined />
+                                </Space>
                             </span>
                         </Dropdown>
-
                     </div>
                 </div>
             </div>
